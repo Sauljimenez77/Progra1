cea7fa206a7e27381589376b11af32ff90e504d0
saúljimenez77 <saul111jimenez@gmail.com>
2025-08-13 19:34:17 -0600

    Este dia hicimos una calculadora con diferentes opciones para resolver ejercicios matematicos

--- miPrimerPryectoCsharp/miPrimerPryectoCsharp/Form1.Designer.cs
+++ miPrimerPryectoCsharp/miPrimerPryectoCsharp/Form1.Designer.cs
@@ -28,78 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblnum1 = new System.Windows.Forms.Label();
-            this.txtnum1 = new System.Windows.Forms.TextBox();
-            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnCalcularopciones = new System.Windows.Forms.Button();
+            this.cboOpciones = new System.Windows.Forms.ComboBox();
+            this.gbOpciones = new System.Windows.Forms.GroupBox();
+            this.optPorcentaje = new System.Windows.Forms.RadioButton();
+            this.optExponente = new System.Windows.Forms.RadioButton();
+            this.optDivision = new System.Windows.Forms.RadioButton();
+            this.optMultiplicacion = new System.Windows.Forms.RadioButton();
+            this.optResta = new System.Windows.Forms.RadioButton();
+            this.optSuma = new System.Windows.Forms.RadioButton();
+            this.lblrespuesta = new System.Windows.Forms.Label();
             this.txtnum2 = new System.Windows.Forms.TextBox();
             this.lblnum2 = new System.Windows.Forms.Label();
-            this.lblrespuesta = new System.Windows.Forms.Label();
+            this.btncantidad = new System.Windows.Forms.Button();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.lblnum1 = new System.Windows.Forms.Label();
+            this.gbOpciones.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblnum1
+            // btnCalcularopciones
             // 
-            this.lblnum1.AutoSize = true;
-            this.lblnum1.Location = new System.Drawing.Point(32, 45);
-            this.lblnum1.Name = "lblnum1";
-            this.lblnum1.Size = new System.Drawing.Size(38, 13);
-            this.lblnum1.TabIndex = 0;
-            this.lblnum1.Text = "Num1:";
+            this.btnCalcularopciones.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcularopciones.Location = new System.Drawing.Point(430, 127);
+            this.btnCalcularopciones.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCalcularopciones.Name = "btnCalcularopciones";
+            this.btnCalcularopciones.Size = new System.Drawing.Size(80, 29);
+            this.btnCalcularopciones.TabIndex = 17;
+            this.btnCalcularopciones.Text = "Calcular";
+            this.btnCalcularopciones.UseVisualStyleBackColor = true;
+            this.btnCalcularopciones.Click += new System.EventHandler(this.btnCalcularopciones_Click);
             // 
-            // txtnum1
+            // cboOpciones
             // 
-            this.txtnum1.Location = new System.Drawing.Point(104, 38);
-            this.txtnum1.Name = "txtnum1";
-            this.txtnum1.Size = new System.Drawing.Size(100, 20);
-            this.txtnum1.TabIndex = 1;
+            this.cboOpciones.FormattingEnabled = true;
+            this.cboOpciones.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicacion",
+            "Divicion",
+            "Exponente",
+            "Porcentaje"});
+            this.cboOpciones.Location = new System.Drawing.Point(429, 86);
+            this.cboOpciones.Margin = new System.Windows.Forms.Padding(2);
+            this.cboOpciones.Name = "cboOpciones";
+            this.cboOpciones.Size = new System.Drawing.Size(82, 21);
+            this.cboOpciones.TabIndex = 16;
             // 
-            // btncalcular
+            // gbOpciones
             // 
-            this.btncalcular.Location = new System.Drawing.Point(113, 136);
-            this.btncalcular.Name = "btncalcular";
-            this.btncalcular.Size = new System.Drawing.Size(118, 60);
-            this.btncalcular.TabIndex = 2;
-            this.btncalcular.Text = "Calcular";
-            this.btncalcular.UseVisualStyleBackColor = true;
-            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            this.gbOpciones.Controls.Add(this.optPorcentaje);
+            this.gbOpciones.Controls.Add(this.optExponente);
+            this.gbOpciones.Controls.Add(this.optDivision);
+            this.gbOpciones.Controls.Add(this.optMultiplicacion);
+            this.gbOpciones.Controls.Add(this.optResta);
+            this.gbOpciones.Controls.Add(this.optSuma);
+            this.gbOpciones.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbOpciones.Location = new System.Drawing.Point(266, 77);
+            this.gbOpciones.Margin = new System.Windows.Forms.Padding(2);
+            this.gbOpciones.Name = "gbOpciones";
+            this.gbOpciones.Padding = new System.Windows.Forms.Padding(2);
+            this.gbOpciones.Size = new System.Drawing.Size(131, 160);
+            this.gbOpciones.TabIndex = 15;
+            this.gbOpciones.TabStop = false;
+            this.gbOpciones.Text = "Opciones";
             // 
-            // txtnum2
+            // optPorcentaje
             // 
-            this.txtnum2.Location = new System.Drawing.Point(104, 83);
-            this.txtnum2.Name = "txtnum2";
-            this.txtnum2.Size = new System.Drawing.Size(100, 20);
-            this.txtnum2.TabIndex = 4;
+            this.optPorcentaje.AutoSize = true;
+            this.optPorcentaje.Location = new System.Drawing.Point(12, 126);
+            this.optPorcentaje.Margin = new System.Windows.Forms.Padding(2);
+            this.optPorcentaje.Name = "optPorcentaje";
+            this.optPorcentaje.Size = new System.Drawing.Size(91, 21);
+            this.optPorcentaje.TabIndex = 5;
+            this.optPorcentaje.TabStop = true;
+            this.optPorcentaje.Text = "Porcentaje";
+            this.optPorcentaje.UseVisualStyleBackColor = true;
             // 
-            // lblnum2
+            // optExponente
             // 
-            this.lblnum2.AutoSize = true;
-            this.lblnum2.Location = new System.Drawing.Point(32, 90);
-            this.lblnum2.Name = "lblnum2";
-            this.lblnum2.Size = new System.Drawing.Size(38, 13);
-            this.lblnum2.TabIndex = 3;
-            this.lblnum2.Text = "Num2:";
+            this.optExponente.AutoSize = true;
+            this.optExponente.Location = new System.Drawing.Point(12, 104);
+            this.optExponente.Margin = new System.Windows.Forms.Padding(2);
+            this.optExponente.Name = "optExponente";
+            this.optExponente.Size = new System.Drawing.Size(90, 21);
+            this.optExponente.TabIndex = 4;
+            this.optExponente.TabStop = true;
+            this.optExponente.Text = "Exponente";
+            this.optExponente.UseVisualStyleBackColor = true;
+            // 
+            // optDivision
+            // 
+            this.optDivision.AutoSize = true;
+            this.optDivision.Location = new System.Drawing.Point(12, 82);
+            this.optDivision.Margin = new System.Windows.Forms.Padding(2);
+            this.optDivision.Name = "optDivision";
+            this.optDivision.Size = new System.Drawing.Size(76, 21);
+            this.optDivision.TabIndex = 3;
+            this.optDivision.Text = "Division";
+            this.optDivision.UseVisualStyleBackColor = true;
+            // 
+            // optMultiplicacion
+            // 
+            this.optMultiplicacion.AutoSize = true;
+            this.optMultiplicacion.Location = new System.Drawing.Point(12, 62);
+            this.optMultiplicacion.Margin = new System.Windows.Forms.Padding(2);
+            this.optMultiplicacion.Name = "optMultiplicacion";
+            this.optMultiplicacion.Size = new System.Drawing.Size(112, 21);
+            this.optMultiplicacion.TabIndex = 2;
+            this.optMultiplicacion.Text = "Multiplicacion";
+            this.optMultiplicacion.UseVisualStyleBackColor = true;
+            // 
+            // optResta
+            // 
+            this.optResta.AutoSize = true;
+            this.optResta.Location = new System.Drawing.Point(12, 37);
+            this.optResta.Margin = new System.Windows.Forms.Padding(2);
+            this.optResta.Name = "optResta";
+            this.optResta.Size = new System.Drawing.Size(61, 21);
+            this.optResta.TabIndex = 1;
+            this.optResta.Text = "Resta";
+            this.optResta.UseVisualStyleBackColor = true;
+            // 
+            // optSuma
+            // 
+            this.optSuma.AutoSize = true;
+            this.optSuma.Checked = true;
+            this.optSuma.Location = new System.Drawing.Point(12, 17);
+            this.optSuma.Margin = new System.Windows.Forms.Padding(2);
+            this.optSuma.Name = "optSuma";
+            this.optSuma.Size = new System.Drawing.Size(61, 21);
+            this.optSuma.TabIndex = 0;
+            this.optSuma.TabStop = true;
+            this.optSuma.Text = "Suma";
+            this.optSuma.UseVisualStyleBackColor = true;
             // 
             // lblrespuesta
             // 
             this.lblrespuesta.AutoSize = true;
-            this.lblrespuesta.Location = new System.Drawing.Point(32, 136);
+            this.lblrespuesta.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblrespuesta.Location = new System.Drawing.Point(48, 121);
+            this.lblrespuesta.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblrespuesta.Name = "lblrespuesta";
-            this.lblrespuesta.Size = new System.Drawing.Size(64, 13);
-            this.lblrespuesta.TabIndex = 5;
-            this.lblrespuesta.Text = "Respuesta?";
+            this.lblrespuesta.Size = new System.Drawing.Size(87, 17);
+            this.lblrespuesta.TabIndex = 14;
+            this.lblrespuesta.Text = "Respuesta: ?";
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(96, 94);
+            this.txtnum2.Margin = new System.Windows.Forms.Padding(2);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(109, 20);
+            this.txtnum2.TabIndex = 13;
+            // 
+            // lblnum2
+            // 
+            this.lblnum2.AutoSize = true;
+            this.lblnum2.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnum2.Location = new System.Drawing.Point(48, 96);
+            this.lblnum2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblnum2.Name = "lblnum2";
+            this.lblnum2.Size = new System.Drawing.Size(49, 17);
+            this.lblnum2.TabIndex = 12;
+            this.lblnum2.Text = "Num2:";
+            // 
+            // btncantidad
+            // 
+            this.btncantidad.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncantidad.Location = new System.Drawing.Point(78, 151);
+            this.btncantidad.Margin = new System.Windows.Forms.Padding(2);
+            this.btncantidad.Name = "btncantidad";
+            this.btncantidad.Size = new System.Drawing.Size(80, 29);
+            this.btncantidad.TabIndex = 11;
+            this.btncantidad.Text = "Calcular";
+            this.btncantidad.UseVisualStyleBackColor = true;
+            this.btncantidad.Click += new System.EventHandler(this.btncantidad_Click);
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(96, 60);
+            this.txtnum1.Margin = new System.Windows.Forms.Padding(2);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(109, 20);
+            this.txtnum1.TabIndex = 10;
+            // 
+            // lblnum1
+            // 
+            this.lblnum1.AutoSize = true;
+            this.lblnum1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnum1.Location = new System.Drawing.Point(48, 60);
+            this.lblnum1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblnum1.Name = "lblnum1";
+            this.lblnum1.Size = new System.Drawing.Size(49, 17);
+            this.lblnum1.TabIndex = 9;
+            this.lblnum1.Text = "Num1:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(467, 265);
+            this.ClientSize = new System.Drawing.Size(589, 314);
+            this.Controls.Add(this.btnCalcularopciones);
+            this.Controls.Add(this.cboOpciones);
+            this.Controls.Add(this.gbOpciones);
             this.Controls.Add(this.lblrespuesta);
             this.Controls.Add(this.txtnum2);
             this.Controls.Add(this.lblnum2);
-            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.btncantidad);
             this.Controls.Add(this.txtnum1);
             this.Controls.Add(this.lblnum1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.gbOpciones.ResumeLayout(false);
+            this.gbOpciones.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,12 +248,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblnum1;
-        private System.Windows.Forms.TextBox txtnum1;
-        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnCalcularopciones;
+        private System.Windows.Forms.ComboBox cboOpciones;
+        private System.Windows.Forms.GroupBox gbOpciones;
+        private System.Windows.Forms.RadioButton optPorcentaje;
+        private System.Windows.Forms.RadioButton optExponente;
+        private System.Windows.Forms.RadioButton optDivision;
+        private System.Windows.Forms.RadioButton optMultiplicacion;
+        private System.Windows.Forms.RadioButton optResta;
+        private System.Windows.Forms.RadioButton optSuma;
+        private System.Windows.Forms.Label lblrespuesta;
         private System.Windows.Forms.TextBox txtnum2;
         private System.Windows.Forms.Label lblnum2;
-        private System.Windows.Forms.Label lblrespuesta;
+        private System.Windows.Forms.Button btncantidad;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.Label lblnum1;
     }
 }
 
--- miPrimerPryectoCsharp/miPrimerPryectoCsharp/Form1.cs
+++ miPrimerPryectoCsharp/miPrimerPryectoCsharp/Form1.cs
@@ -35,7 +35,77 @@ namespace miPrimerPryectoCsharp
 
             lblrespuesta.Text = "Respuesta:" + respuesta;
         }
-        
 
+        private void btncantidad_Click(object sender, EventArgs e)
+        {
+            double num1, num2, respuesta = 0;
+            num1 = Double.Parse(txtnum1.Text);
+            num2 = Double.Parse(txtnum2.Text);
+            if (optSuma.Checked)
+            {
+                respuesta = num1 + num2;
+            }
+
+            if (optResta.Checked)
+            {
+                respuesta = num1 - num2;
+            }
+
+            if (optMultiplicacion.Checked)
+            {
+                respuesta = num1 * num2;
+            }
+
+            if (optDivision.Checked)
+            {
+                respuesta = num1 / num2;
+            }
+
+            if (optExponente.Checked)
+            {
+                respuesta = Math.Pow(num1, num2);
+            }
+
+
+            if (optPorcentaje.Checked)
+            {
+                respuesta = num1 % num2;
+            }
+
+            //Porcentaje
+            //Factorial. 5! = 5x4x3x3x1=120
+            //Modulo
+
+            lblrespuesta.Text = "Respuesta: " + respuesta;
+
+        }
+
+        private void btnCalcularopciones_Click(object sender, EventArgs e)
+        {
+            double num1, num2, respuesta = 0;
+            num1 = double.Parse(txtnum1.Text);
+            num2 = double.Parse(txtnum2.Text);
+            switch (cboOpciones.SelectedIndex)
+            {
+                case 0:
+                    respuesta = num1 + num2;
+                    break;
+                case 1:
+                    respuesta = num1 - num2;
+                    break;
+                case 2:
+                    respuesta = num1 * num2;
+                    break;
+                case 3:
+                    respuesta = Math.Pow(num1, num2);
+                    break;
+
+                case 4:
+                    respuesta = num1 % num2;
+                    break;
+
+            }
+            lblrespuesta.Text = "Respuesta: " + respuesta;
+        }
     }
 }
